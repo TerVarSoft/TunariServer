38bfdc5d72a2451c93f19911f94f9ccb801c2e43
Mirko Terrazas <mirko.jterrazasv@gmail.com>
2016-12-26 21:15:17 -0400

    added server

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var app = require('./app');
+
+var port = process.env.PORT || 5000;
+
+var server = app.listen(port, function () {
+  console.log('Tunari Server is running at %s port', port);
+});
